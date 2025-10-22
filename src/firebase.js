59316682c53ef146a0324ec40eefaa85481f466d
KarruHarin <harinkarru@gmail.com>
2025-10-22 23:27:22 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getDatabase} from "firebase/database"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2ybQxylDCSk6KCU4WZw0JU9BZUb6UjOo",
-  authDomain: "kbc-game-13588.firebaseapp.com",
-  databaseURL: "https://kbc-game-13588-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "kbc-game-13588",
-  storageBucket: "kbc-game-13588.firebasestorage.app",
-  messagingSenderId: "1013045720133",
-  appId: "1:1013045720133:web:49d548bcc4e059ec001f2c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const db = getDatabase(app);
